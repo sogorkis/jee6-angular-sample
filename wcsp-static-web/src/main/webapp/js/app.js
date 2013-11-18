c5a704bb0117d3f66e79372d97428e2a2cf6efe2
sogorkis <s.ogorkis@gmail.com>
2013-11-17 20:09:12 -0800

    Changed rules for showing loading message.

--- wcsp-static-web/src/main/webapp/js/app.js
+++ wcsp-static-web/src/main/webapp/js/app.js
@@ -4,31 +4,46 @@
 // Declare app level module which depends on filters, and services
 var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', 'ngRoute', 'ngCookies', 'ui.bootstrap'])
     .config(['$routeProvider', '$provide', '$httpProvider', function ($routeProvider, $provide, $httpProvider) {
-        $routeProvider.when('/', {templateUrl: 'partials/home.html'});
-        $routeProvider.when('/about', {templateUrl: 'partials/about.html'});
-        $routeProvider.when('/try', {templateUrl: 'partials/try.html', controller: 'TryCtrl'});
-        $routeProvider.when('/register', {templateUrl: 'partials/register.html', controller: 'RegisterCtrl'});
-        $routeProvider.when('/requests', {templateUrl: 'partials/requests.html', controller: 'RequestsCtrl'});
-        $routeProvider.when('/error', {templateUrl: 'partials/error.html'});
-        $routeProvider.when('/error403', {templateUrl: 'partials/error403.html'});
+        $routeProvider.when('/', {templateUrl: '/partials/home.html'});
+        $routeProvider.when('/about', {templateUrl: '/partials/about.html'});
+        $routeProvider.when('/try', {templateUrl: '/partials/try.html', controller: 'TryCtrl'});
+        $routeProvider.when('/register', {templateUrl: '/partials/register.html', controller: 'RegisterCtrl'});
+        $routeProvider.when('/requests', {templateUrl: '/partials/requests.html', controller: 'RequestsCtrl'});
+        $routeProvider.when('/error', {templateUrl: '/partials/error.html'});
+        $routeProvider.when('/error403', {templateUrl: '/partials/error403.html'});
         $routeProvider.otherwise({redirectTo: '/view1'});
 
         $provide.factory('myHttpInterceptor', ['$q', '$rootScope', 'GlobalErrors', function ($q, $rootScope, GlobalErrors) {
             var numLoadings = 0;
 
+            var shouldShowLoader = function (config) {
+                // do not show url loader on angular partials templates
+                if (config.url.substr(0, 10) === '/partials/') {
+                    return false;
+                }
+                // do not show url loader on getting logged user data
+                if (config.url === '/rest/auth/getAppUser') {
+                    return false;
+                }
+                return true;
+            };
+
             return {
                 request: function (config) {
 
-                    numLoadings++;
+                    if (shouldShowLoader(config)) {
+                        console.log('url: ' + config.url);
+                        numLoadings++;
+                        // Show loader
+                        $rootScope.$broadcast("loader_show");
 
-                    // Show loader
-                    $rootScope.$broadcast("loader_show");
+                    }
                     return config || $q.when(config)
 
                 },
                 response: function (response) {
 
-                    if ((--numLoadings) === 0) {
+                    if (shouldShowLoader(response.config) && (--numLoadings) === 0) {
                         // Hide loader
                         $rootScope.$broadcast("loader_hide");
                     }
@@ -40,7 +55,7 @@ var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.d
 
                     GlobalErrors.handleHttpErrorResponse(response);
 
-                    if (!(--numLoadings)) {
+                    if (shouldShowLoader(response.config) && !(--numLoadings)) {
                         // Hide loader
                         $rootScope.$broadcast("loader_hide");
                     }
